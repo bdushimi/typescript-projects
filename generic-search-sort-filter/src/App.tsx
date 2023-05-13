5b4881e35931d57b7accf0f0c09dc4ebf3ad5f6a
bdushimi <lespharaons@hotmail.com>
2023-05-13 14:58:14 +0200

    Adds generic search fuctionality

--- generic-search-sort-filter/src/App.tsx
+++ generic-search-sort-filter/src/App.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import {widgets} from './mock-data/widget';
 import {people} from './mock-data/people';
 import {genericSearch} from './utils/genericSearch';
+import {SearchInput} from './components/SearchInput';
 
 function App() {
+  const [query, setQuery] = useState('');
   return (
     <>
+      <SearchInput setSearchQuery={setQuery} />
       <h2>Widgets: </h2>
-      {widgets.map(widget => (
-        <h3 key={widget.id}>{widget.title}</h3>
-      ))}
+      {widgets
+        .filter(widget =>
+          genericSearch(widget, ['title', 'description'], query, false)
+        )
+        .map(widget => (
+          <h3 key={widget.id}>{widget.title}</h3>
+        ))}
       <h2>People: </h2>
-      {people.map((person, index) => (
-        <h3 key={index}>
-          {person.firstName} {person.lastName}
-        </h3>
-      ))}
+      {people
+        .filter(person =>
+          genericSearch(
+            person,
+            ['firstName', 'lastName', 'eyeColor'],
+            query,
+            false
+          )
+        )
+        .map((person, index) => (
+          <h3 key={index}>
+            {person.firstName} {person.lastName}
+          </h3>
+        ))}
     </>
   );
 }
